6b2d72ccb9ca3f597cc1aacdead18ed4b82e0a64
DanyloTymets <56023366+DanyloTymets@users.noreply.github.com>
2019-12-19 18:31:44 +0200

    Added comments section

--- details.php
+++ details.php
@@ -282,6 +282,18 @@ include_once("functions/functions.php");
                    ?>
                </div>
            </div>
+           <!--<div id="disqus_thread"></div>
+            <script>
+            (function() {
+            var d = document, s = d.createElement('script');
+            s.src = 'https://http-localhost-horondiecommerceweb.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+            })();
+            </script>-->
+            <div id="fb-root"></div>
+            <script async defer crossorigin="anonymous" src="https://connect.facebook.net/uk_UA/sdk.js#xfbml=1&version=v5.0&appId=1273845056156438&autoLogAppEvents=1"></script>
+            <div class="fb-comments" data-href="http://localhost/webshop-main/details.php" data-width="" data-numposts="5"></div>
        </div>
    </div>
    
--- sidebar.php
+++ sidebar.php
@@ -17,6 +17,60 @@ if(isset($_REQUEST['p_cat'])&&is_array($_REQUEST['p_cat'])){
     }
 }
 ?>
+
+<div class="panel panel-default sidebar-menu">
+    <div class="panel-heading">
+        <h3 class="panel-title">
+            Product Categories
+            <div class="pull-right">
+                <a href="JavaScript:Void(0);" style="color:black;">
+                    <span class="nav-toggle hide-show">
+                        Hide
+                    </span>
+                </a>
+            </div>
+        </h3>
+    </div>
+    <div class="panel-collapse collapse-data">
+        <div class="panel-body"> 
+            <div class="input-group"> 
+                <input type="text" class="form-control" id="dev-table-filter" data-filters="#dev-p-cat" data-action="filter" placeholder="Filter Product Categories">
+                    <a class="input-group-addon">
+                        <i class="fa fa-search"></i>
+                    </a>
+            </div>
+            </div> 
+        <div class="panel-body scroll-menu"> 
+            <ul class="nav nav-pills nav-stacked category-menu" id="dev-p-cat">
+                <?php
+                $get_p_cat = "select * from product_categories";
+                $run_p_cat = mysqli_query($con,$get_p_cat);
+                while($row_p_cat=mysqli_fetch_array($run_p_cat)){
+                    $p_cat_id = $row_p_cat['p_cat_id'];
+                    $p_cat_title = $row_p_cat['p_cat_title'];
+                    
+                    echo "
+                    <li  class='checkbox checkbox-primary'>
+                        <a>
+                            <label>
+                                <input ";
+                                if(isset($aPcat[$p_cat_id])){
+                                    echo "checked='checked'";
+                                }
+                                echo " value='$p_cat_id' type='checkbox' class='get_p_cat' name='p_cat'>
+                                <span>
+                                $p_cat_title
+                                </span>
+                            </label>
+                        </a>
+                    </li>
+                    ";
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+</div>
 <div class="panel panel-default sidebar-menu"> 
     <div class="panel-heading"> 
         <h3 class="panel-title">
@@ -67,59 +121,6 @@ if(isset($_REQUEST['p_cat'])&&is_array($_REQUEST['p_cat'])){
                 }
                 ?>
                 
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="panel panel-default sidebar-menu">
-    <div class="panel-heading">
-        <h3 class="panel-title">
-            Product Categories
-            <div class="pull-right">
-                <a href="JavaScript:Void(0);" style="color:black;">
-                    <span class="nav-toggle hide-show">
-                        Hide
-                    </span>
-                </a>
-            </div>
-        </h3>
-    </div>
-    <div class="panel-collapse collapse-data">
-        <div class="panel-body"> 
-            <div class="input-group"> 
-                <input type="text" class="form-control" id="dev-table-filter" data-filters="#dev-p-cat" data-action="filter" placeholder="Filter Product Categories">
-                    <a class="input-group-addon">
-                        <i class="fa fa-search"></i>
-                    </a>
-            </div>
-            </div> 
-        <div class="panel-body scroll-menu"> 
-            <ul class="nav nav-pills nav-stacked category-menu" id="dev-p-cat">
-                <?php
-                $get_p_cat = "select * from product_categories";
-                $run_p_cat = mysqli_query($con,$get_p_cat);
-                while($row_p_cat=mysqli_fetch_array($run_p_cat)){
-                    $p_cat_id = $row_p_cat['p_cat_id'];
-                    $p_cat_title = $row_p_cat['p_cat_title'];
-                    
-                    echo "
-                    <li  class='checkbox checkbox-primary'>
-                        <a>
-                            <label>
-                                <input ";
-                                if(isset($aPcat[$p_cat_id])){
-                                    echo "checked='checked'";
-                                }
-                                echo " value='$p_cat_id' type='checkbox' class='get_p_cat' name='p_cat'>
-                                <span>
-                                $p_cat_title
-                                </span>
-                            </label>
-                        </a>
-                    </li>
-                    ";
-                }
-                ?>
             </ul>
         </div>
     </div>
